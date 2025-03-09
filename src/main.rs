058649ec17e36d46550020978e87277e908f8e4f
dfayd <78728332+dfayd0@users.noreply.github.com>
2025-03-10 00:57:56 +0100

    removing old templates, adding trace and a wraper for the askama templates

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 mod routes;
 
-use axum::response::Redirect;
-use axum::routing::{get, post};
-
-use axum::Router;
+use axum::{
+    response::Redirect,
+    routing::get,
+    Router,
+};
 use tower_http::services::ServeDir;
+use tracing::info;
+use tracing_subscriber::{
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+};
 
 #[tokio::main]
 async fn main()
 {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "listem=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    info!("Initializing server...");
     let app = Router::new()
         .route("/", get(|| async { Redirect::permanent("/home") }))
         .route("/home", get(routes::home))
@@ -16,9 +31,12 @@ async fn main()
 
     let addr = "0.0.0.0";
     let port = 4444;
+
     let listener: tokio::net::TcpListener =
         tokio::net::TcpListener::bind(format!("{addr}:{port}").as_str())
             .await
             .unwrap();
+
+    info!("Server listening on http://{addr}:{port}");
     axum::serve(listener, app).await.unwrap();
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,46 @@
-use axum::response::{Html, Response};
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{
+        Html,
+        IntoResponse,
+        Response,
+    },
+};
 
-pub async fn home() -> Html<&'static str>
+/// A wrapper type that we'll use to encapsulate HTML parsed by askama into
+/// valid HTML for axum to serve.
+struct HtmlTemplate<T>(T);
+
+/// Allows us to convert Askama HTML templates into valid HTML for axum to serve
+/// in the response.
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
 {
-    Html("<p>Hello, World!</p>")
+    fn into_response(self) -> Response
+    {
+        // Attempt to render the template with askama
+        match self.0.render() {
+            // If we're able to successfully parse and aggregate the template,
+            // serve it
+            Ok(html) => Html(html).into_response(),
+            // If we're not, return an error or some bit of fallback HTML
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
+}
+
+#[derive(Template)]
+#[template(path = "home.html")]
+struct HomeTemplate;
+
+pub async fn home() -> impl IntoResponse
+{
+    let template = HomeTemplate {};
+    HtmlTemplate(template)
 }
