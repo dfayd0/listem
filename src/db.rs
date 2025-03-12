591a0eafde20864edc3186265e219a33a484d9f1
dfayd <78728332+dfayd0@users.noreply.github.com>
2025-03-12 17:04:03 +0100

    feat: working create_db_pool

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,12 @@
-use std::env;
-
-use diesel::prelude::*;
-use dotenvy::dotenv;
+use ::dotenvy::dotenv;
+use ::std::env;
+use diesel::{
+    prelude::*,
+    r2d2::{
+        ConnectionManager,
+        Pool,
+    },
+};
 
 pub fn establish_connection() -> SqliteConnection
 {
@@ -11,6 +16,20 @@ pub fn establish_connection() -> SqliteConnection
         env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_db_pool() -> Pool<ConnectionManager<SqliteConnection>>
+{
+    dotenv().ok();
+
+    let database_url =
+        env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+    Pool::builder()
+        .test_on_check_out(true)
+        .build(manager)
+        .expect("Failed to create pool")
 }
 
 use crate::models::{
